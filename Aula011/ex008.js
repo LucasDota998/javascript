c37e9b20f8e38b97fb198660b840562203a997b3
LucasDota998 <lsdota@gmail.com>
2023-06-21 22:47:57 +0200

    comitei

---
+++ Aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log (`A velocidade do seu carro é ${vel} km/h`)
+if(vel>60){ //condição simples
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija com cuidado!`)
---
+++ Aula12/ex011.js
@@ -0,0 +1,13 @@
+var idade = 10
+console.log(`Voce tem ${idade} anos`)
+if(idade<16){
+    console.log('Não vota')
+}  else if(idade<18 || idade> 65){
+    console.log('Voto opicional')
+
+}   else{ 
+    console.log('Voto Obrigatório')
+
+}
+    
+
